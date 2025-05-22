68275dd616f398a68effbf503563e881123e95bc
AlexCleberLimaFerreira <alexcleberlimaferreira@gmail.com>
2025-05-22 15:24:37 -0300

    botão q muda de cor

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
 import { useState,useEffect, use } from 'react';
+import './src/components/App.css';
+import { useState } from 'react';
 
 
 export default function App() {
 
-  const [mensagem1, mudarMensagem1] = useState('Hello World!');  // Estado inicial da mensagem1 é "Hello World!"
-  const [mensagem2, mudarMensagem2] = useState('Hello World!');
+  const [valor, mudarValor] = useState(true);
 
-  useEffect(() => {
-    console.log('Executando na renderização');
-  },[mensagem1,mensagem2]); // O useEffect é chamado sempre que a mensagem1 ou mensagem2 muda no butão
-
+  function alterarValor() {
+    mudarValor(!valor);
+  }
 
   return (
-    <>
-      <h1>{mensagem1}</h1>
-      <button onClick={() => {
-        mudarMensagem1('Olá Mundo1!');
-      }}>Mudar Idioma</button>
-
-
-      <h1>{mensagem2}</h1>
-      <button onClick={() => {
-        mudarMensagem2('Olá Mundo2!');
-      }}>Mudar Idioma</button>
-    </>
+    <div style={{ backgroundColor: valor ? 'blue' : 'red' }}>
+    <button style={{ backgroundColor: valor ? 'blue' : 'red' }} onClick={alterarValor}>CLIQUE!</button>
+    </div>
   )
-}
+};
